d9be91175b5ec906831ea84e8e17ec8cdb9cfc1a
dr.max <maximilien@acm.org>
2014-10-27 09:46:36 -0700

    added softlayer specific entries

--- spec/run-bats.sh
+++ spec/run-bats.sh
@@ -16,21 +16,21 @@ spec_path=$PWD/bat.spec
 # Create bat.spec used by BATS to generate BOSH manifest
 cat > $spec_path << EOF
 ---
-cpi: warden
+cpi: softlayer
 properties:
   static_ip: 10.244.0.2
   uuid: $director_uuid
   pool_size: 1
   instances: 1
   stemcell:
-    name: bosh-warden-boshlite-ubuntu-trusty-go_agent
+    name: bosh-softlayer-ubuntu-trusty-go_agent
     version: latest
   mbus: nats://nats:0b450ada9f830085e2cdeff6@10.42.49.80:4222
 EOF
 
 # Set up env vars used by BATS
 export BAT_DEPLOYMENT_SPEC=$spec_path
-export BAT_STEMCELL=$HOME/Downloads/bosh-stemcell-3-warden-boshlite-ubuntu-trusty-go_agent.tgz
+export BAT_STEMCELL=$HOME/Downloads/bosh-stemcell-3-softlayer-ubuntu-trusty-go_agent.tgz
 export BAT_DIRECTOR=$director_target
 export BAT_DNS_HOST=$director_target
 export BAT_VCAP_PASSWORD=c1oudc0w
